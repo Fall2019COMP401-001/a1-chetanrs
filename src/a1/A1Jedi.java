4628314f605d58efb0788d15485310e2b78f6369
Chetan <41928408+CSingalreddy01@users.noreply.github.com>
2019-08-29 17:39:51 -0400

    Finished Jedi level

--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -1,14 +1,68 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class A1Jedi {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
+		 
+		int itemsInStore = scan.nextInt();
+		objectInStore[] inventory = new objectInStore[itemsInStore];
 		
+		for (int i = 0; i < itemsInStore; i++) {
+			String itemName = scan.next();
+			double itemPrice = scan.nextDouble();
+			
+			inventory[i] = new objectInStore(itemName);
+			
+		}
+		
+		int customerCount = scan.nextInt();
+		
+		for (int i = 0; i < customerCount; i++) {
+			String fullName = scan.next() + " " + scan.next();
+			int customerProducts = scan.nextInt();
+			
+			for (int j = 0; j < customerProducts; j++) {
+				int customerItemCount = scan.nextInt();
+				String customerItemName = scan.next();
+				
+				for (int k = 0; k < itemsInStore; k++) {
+					if (inventory[k].name.equals(customerItemName)) {
+						inventory[k].numberSold += customerItemCount;
+						inventory[k].totalCustomers += 1;
+					}
+				}
+			}
+		}
+		
+		for (int i = 0; i < itemsInStore; i++) {
+			objectInStore currentItem = inventory[i];
+			
+			if (currentItem.totalCustomers == 0) {
+				System.out.println("No customers bought " + currentItem.name);
+			} else {
+				System.out.println(currentItem.totalCustomers + " customers bought " + currentItem.numberSold + " " + currentItem.name);
+			}
+		}
+		
+		
+	}
+}
+
+class objectInStore {
+	String name;
+	int numberSold;
+	int totalCustomers;
+	
+	public objectInStore (String itemName) {
+		name = itemName;
+		numberSold = 0;
+		totalCustomers = 0;
 	}
 }
