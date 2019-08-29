d15cf704da7d5cacac85c13a228f887a057ea19b
Chetan <41928408+CSingalreddy01@users.noreply.github.com>
2019-08-29 12:54:54 -0400

    Finished adept/novice levels, submitting to gradescope

--- src/a1/A1Adept.java
+++ src/a1/A1Adept.java
@@ -1,14 +1,66 @@
 package a1;
 
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class A1Adept {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		int numberOfItems = scan.nextInt();
+		Map<String, Double> items = new HashMap<String, Double>();
+		
+		for (int i = 0; i < numberOfItems; i++) {
+			String item = scan.next();
+			double price = scan.nextDouble();
+			items.put(item, price);
+			
+		}	
+		
+		int numberOfCustomers = scan.nextInt();
+		Map<String, Double> customers = new HashMap<String, Double>();
+		
+		String maxName = null;
+		double maxPurchase = 0;
+		String minName = null;
+		double minPurchase = 10000;
+		double averageTotal = 0;
+		
+		for (int i = 0; i < numberOfCustomers; i++) {
+			String fName = scan.next();
+			String lName = scan.next();
+			String fullName = fName + " " + lName;
+			double total = 0;
+			
+			int purchaseCount = scan.nextInt();
+			
+			for (int j = 0; j < purchaseCount; j++) {
+				int itemCount = scan.nextInt();
+				String itemName = scan.next();
+				total += itemCount * items.get(itemName);
+				
+				if (total > maxPurchase) {
+					maxPurchase = total;
+					maxName = fullName;
+				} else if (total < minPurchase) {
+					minPurchase = total;
+					minName = fullName;
+				}
+			}
+			
+			customers.put(fullName, total);
+			averageTotal += total;
+		}
+		
+		double average = averageTotal / numberOfCustomers;
+		
+		System.out.println("Biggest: " + maxName + " (" + String.format("%.2f", customers.get(maxName)) + ")");
+		System.out.println("Smallest: " + minName + " (" + String.format("%.2f", customers.get(minName)) + ")");	
+		System.out.println("Average: " + String.format("%.2f", average));
 	}
+		
 }
+
--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,32 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+
+		int numberOfCustomers = scan.nextInt();
+		String[] output = new String[numberOfCustomers];
+		
+		for (int i = 0; i < numberOfCustomers; i++) {
+			String fName = scan.next();
+			String lName = scan.next();
+			int numberOfItems = scan.nextInt();
+			
+			double total = 0;
+			
+			for (int j = 0; j < numberOfItems; j++) {
+				double quantity = scan.nextDouble();
+				String item = scan.next();
+				double price = scan.nextDouble();
+				
+				total += quantity * price;
+				
+			}
+			
+			output[i] = fName.charAt(0) + "." + " " + lName + ":" + " " + String.format("%.2f", total);
+		}
+		
+		for (int i = 0; i < numberOfCustomers; i++) {
+			System.out.println(output[i]);
+		}
 		
 	}
 }
